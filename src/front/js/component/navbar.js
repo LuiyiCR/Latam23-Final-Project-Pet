8d6ebf46b7e3c25f9c3ae798acef7683a1e74d03
Luis Salazar O <luiyi_salazar@hotmail.com>
2024-01-20 17:48:46 +0000

    Navbar and Hero sections adjust

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,64 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logoImgUrl from '../../img/logo.png';
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <img className="logo" src={logoImgUrl} alt="Pet+ Logo" />
+        </a>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav mx-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Mis mascotas
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Veterinarios
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Acerca de nosotros
+              </a>
+            </li>
+          </ul>
+
+          <div className="d-flex">
+            <a className="btn btn-outline-primary me-2" href="#">
+              Invitado
+            </a>
+            <a className="btn btn-outline-primary me-2" href="#">
+              Entrar
+            </a>
+            <a className="btn btn-primary" href="#">
+              Registrarse
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,46 @@
-import React, { useContext } from "react";
-import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
+import React, { useContext } from 'react';
+import { Context } from '../store/appContext';
+import rigoImageUrl from '../../img/rigo-baby.jpg';
+import '../../styles/home.css';
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  return (
+    <div className="container-fluid">
+      <div className="hero text-center text-white">
+        <div className="container">
+          <h1>PET+, el mejor aliado en el cuidado de tus mascotas</h1>
+          <h6 className="text-muted">
+            Registra, Controla y Disfruta de la Salud de tus Mascotas con Pet+.
+            Mantén al Día sus Vacunas, Peso, Citas y más, todo en un solo lugar
+            su bienestar merece la mejor atención.
+          </h6>
+        </div>
+
+        <div class="d-grid gap-2">
+          <button class="btn btn-primary" type="button">
+            Invitado
+          </button>
+          <button class="btn btn-secondary" type="button">
+            Usuario
+          </button>
+          <button class="btn btn-secondary" type="button">
+            Registrarse
+          </button>
+        </div>
+
+        <p>
+          <img src={rigoImageUrl} />
+        </p>
+
+        <p>
+          This boilerplate comes with lots of documentation:{' '}
+          <a href="https://start.4geeksacademy.com/starters/react-flask">
+            Read documentation
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 };
