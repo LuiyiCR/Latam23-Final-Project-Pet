3b8755cad3fb2c43d990f194afa6063c56766c21
Luis Salazar O <luiyi_salazar@hotmail.com>
2024-01-21 22:39:46 +0000

    Set spacing and divs for the home page

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -7,40 +7,41 @@ export const Home = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    <div className="container-fluid">
-      <div className="hero text-center text-white">
+    <div className="container">
+      <div className="hero text-center">
         <div className="container">
-          <h1>PET+, el mejor aliado en el cuidado de tus mascotas</h1>
-          <h6 className="text-muted">
-            Registra, Controla y Disfruta de la Salud de tus Mascotas con Pet+.
-            Mantén al Día sus Vacunas, Peso, Citas y más, todo en un solo lugar
+          <h1 className="hero-title">PET+, el mejor aliado en el cuidado de tus mascotas</h1>
+          <p className="lead text-muted">
+            Registra, controla, y disfruta de la salud de tus mascotas con Pet+.
+            Mantén al día sus vacunas, peso, citas y más. Todo en un solo lugar,
             su bienestar merece la mejor atención.
-          </h6>
-        </div>
+          </p>
 
-        <div className="d-grid gap-2">
-          <button className="btn text-white" type="button">
-            Invitado
-          </button>
-          <button className="btn btn-light text-black" type="button">
-            Usuario
-          </button>
-          <button className="btn btn-light text-black" type="button">
-            Registrarse
-          </button>
+          <div className="d-grid gap-2">
+            <button className="btn text-white" type="button">
+              Invitado
+            </button>
+            <button className="btn btn-light text-black" type="button">
+              Usuario
+            </button>
+            <button className="btn btn-light text-black" type="button">
+              Registrarse
+            </button>
+          </div>
+        </div>
+        <div className='img-container'>
+          <p>
+            <img className="img-fluid mt-3 mb-3" src={heroImgUrl} alt='Picture of pets' />
+          </p>
         </div>
 
         <p>
-          <img className="hero-images" src={heroImgUrl} />
-        </p>
-
-        <p>
-          This boilerplate comes with lots of documentation:{' '}
-          <a href="https://start.4geeksacademy.com/starters/react-flask">
-            Read documentation
+          Acá puedes ver el código fuente{' '}
+          <a href="https://github.com/4GeeksAcademy/Latam23-Final-Project-Pet">
+            GITHUB
           </a>
         </p>
       </div>
-    </div>
+    </div >
   );
 };
