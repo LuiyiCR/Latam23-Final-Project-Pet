892009245c88eafaf1280fe2e9f671dab3c0838d
Luis Salazar O <luiyi_salazar@hotmail.com>
2024-01-21 22:47:18 +0000

    Set basic footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,7 +1,30 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-
+	<footer className="footer mt-auto py-3 text-center bg-light">
+		<div className="container">
+			<div className="row">
+				<div className="col-md-4">
+					<h5>Contacto</h5>
+					<p>Correo electrónico: info@petplus.com</p>
+					<p>Teléfono: (123) 456-7890</p>
+				</div>
+				<div className="col-md-4">
+					<h5>Enlaces útiles</h5>
+					<ul className="list-unstyled">
+						<li><a href="#">Términos de servicio</a></li>
+						<li><a href="#">Política de privacidad</a></li>
+					</ul>
+				</div>
+				<div className="col-md-4">
+					<h5>Síguenos</h5>
+					<div className="d-flex justify-content-center">
+						<a href="#" className="me-2"><i className="fab fa-facebook"></i></a>
+						<a href="#" className="me-2"><i className="fab fa-twitter"></i></a>
+						<a href="#" className="me-2"><i className="fab fa-instagram"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
 	</footer>
 );
