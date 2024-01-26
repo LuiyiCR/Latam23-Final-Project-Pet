14d9bad34b008626421f84cb1d9a682342414c15
Miguel Alberto Ordoñez <miguelssj5040@gmail.com>
2024-01-26 05:36:20 +0000

    added new favicon

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -15,7 +15,6 @@ import { Link } from 'react-router-dom';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import { Button } from 'react-bootstrap';
 import '../../styles/home.css';
 
 export const Home = () => {
