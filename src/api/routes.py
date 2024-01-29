2fb1de74bcf779de1a2cd94aafd02c5b35715792
Miguel Alberto Ordoñez <miguelssj5040@gmail.com>
2024-01-29 06:07:56 +0000

    endpoint pet created

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Pet
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 from bcrypt import gensalt
@@ -82,3 +82,29 @@ def login():
             print(error)
             return jsonify({"message": "Server error"}), 500 
             
+
+@api.route('/pet', methods=['POST', 'GET'])
+def handle_pets():
+    if request.method == 'POST':
+        data = request.json
+        name = data.get("name")
+        born_date = data.get("born_date")
+        disabilities = data.get("disabilities")
+        breed = data.get("breed")
+        gender = data.get("gender")
+        animal = data.get("animal")
+        medical_history = data.get("medical_history")
+        # Verificaciones
+        verify_data = [name, born_date, disabilities, breed, gender, animal, medical_history]
+        if None in verify_data:
+            return jsonify({"message":"All parameters are required"}), 400
+        # Creacion de la mascota
+        new_pet = Pet(name = name, born_date = born_date, disabilities = disabilities, breed = breed, gender = gender, animal = animal, medical_history = medical_history)
+        try:
+            db.session.add(new_pet)
+            db.session.commit()
+            return jsonify({"message": "pet successfully registered"}), 201
+        except Exception as error:
+            db.session.rollback()
+            return jsonify({"message": "Server error"}), 500
+        
