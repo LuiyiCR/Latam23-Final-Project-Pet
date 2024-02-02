95be5d6a978ac378803c2eb24c25495245199ec6
Luis Salazar O <luiyi_salazar@hotmail.com>
2024-02-02 01:58:47 +0000

    Refactor Directory component and update dashboard styles

--- src/front/js/component/Directory.js
+++ src/front/js/component/Directory.js
@@ -2,17 +2,19 @@ import React from 'react';
 
 const Directory = ({ veterinaries }) => {
   return (
-    <div>
+    <React.Fragment>
       <h2 className='directory-header'>Directorio de Veterinarios</h2>
-      {veterinaries.map((veterinary, index) => (
-        <div className='test' key={index}>
-          <h3 className='veterinary-name'>{veterinary.name}</h3>
-          <p>{veterinary.phoneNumber}</p>
-          <img className='veterinary-picture' src={veterinary.picture} alt={veterinary.name} />
-          <p>{veterinary.country}</p>
-        </div>
-      ))}
-    </div>
+      <div className='directory-cards'>
+        {veterinaries.map((veterinary, index) => (
+          <div className='directory-card' key={index}>
+            <h3 className='veterinary-name'>{veterinary.name}</h3>
+            <p>{veterinary.phoneNumber}</p>
+            <img className='veterinary-picture' src={veterinary.picture} alt={veterinary.name} />
+            <p>{veterinary.country}</p>
+          </div>
+        ))}
+      </div>
+    </React.Fragment>
   );
 };
 
